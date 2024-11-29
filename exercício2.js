c9c2a1d96f7e752ef85dbd8b304059e25583a951
MuriloBelliAguiar <147894744+MuriloBelliAguiar@users.noreply.github.com>
2024-11-29 13:59:31 -0300

    Add files via upload

---
+++ exercício2.js
@@ -0,0 +1,112 @@
+// 1 fácil
+let nome = "Murilo";
+console.log(nome);
+
+// 2 fácil
+let texto = "string01";
+console.log(texto == "string01")
+
+// 3 fácil
+let = 6;
+console.log(let > 10);
+
+// 4 fácil
+let idade = 21;
+if (idade >= 18){
+    console.log("Parabéns!");
+} else {
+    console.log("Não adulto");
+}
+
+// 5 fácil
+let idadi = 21;
+if (idadi >= 16){
+    console.log("Pode votar!");
+} else {
+    console.log("Não pode votar!");
+}
+
+// 6 fácil
+let numero = 54
+if (numero >=50 && numero <=100){
+    console.log(true);
+} else {
+    (numero <50 && numero > 100)
+    console.log(false);
+}
+
+// 1 médio
+let idadeuser = 35;
+let nomi = "Nome:";
+let ydade = "Idade:";
+console.log(nomi);
+console.log(ydade + idadeuser);
+console.log(idadeuser >=18);
+
+// 2 médio
+let num3 = 22;
+let num4 = 17;
+
+if(num3 > num4) {
+    console.log(num3)
+}else{console.log(num4)};
+
+// 3 médio
+let text1 = "lamborghini";
+if(text1.length > 9){
+    console.log("texto grande")
+    }else{ console.log ("texto pequeno")};
+
+    // 4 médio
+    let numeru = 12
+    let numeral = 15
+    let par = numeru % 2 === 0;
+    let pares = numeral % 2 === 0;
+    console.log(par);
+    console.log(pares);
+
+
+    // 5 médio
+let mstring = "Estou aprendendo Javascript";
+let tsring = mstring.includes("Javascript");
+console.log(tsring);
+
+// 6 médio
+let Pedido = "Introduza seu salário:";
+console.log(Pedido);
+let salario = 4369;
+console.log(salario > 2000);
+
+// 1 difícil
+let senha = "javascript"
+let senha1 = senha.length >= 8;
+let senha2 = senha.includes(1,2,3,4,5,6,7,8,9,0);
+console.log(senha1 && senha2);
+
+// 2 difícil
+
+let nota1 = 1;
+let nota2 = 3;
+let nota3 = 6.3;
+let divisão = (nota1 + nota2 + nota3) /3;
+if( divisão >= 7) {
+    console.log("Aprovado");
+} else {console.log("Reprovado")};
+
+
+// 3 difícil
+let ano = 2100
+if( ano %4 === 0 && ano %100 !== 0){
+    console.log("É bissexto!");
+} else{console.log("Não é bissexto!")};
+
+// 4 difícil
+let nu1 = 692
+let nu2 = 512
+let nu3 = 213
+if(nu1 > nu2 && nu1 > nu3){
+    console.log ("O primeiro número é o maior dos três!")
+}else if( nu1 < nu2 && nu1 < nu3){
+    console.log("O Primeiro número não é o maior!");
+}else{console.log("os três são iguais")}
+
